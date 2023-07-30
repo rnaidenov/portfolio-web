4f34f4b672de81be45533694702f7a218276cf08
Radoslav Naydenov <radoslav.naydenov@homagames.com>
2023-07-30 17:40:35 +0200

    improve transition between pages

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -13,10 +13,13 @@ export const About = () => {
       // and once it exits the viewport, so we check if it is currently
       // intersecting to avoid triggering the animation twice
       if (entries[0].isIntersecting) {
-        gsap.fromTo(navRef.current, { height: '100vh' }, { height: '50px', duration: 0.5, ease: 'power2.inOut' });
+        gsap.fromTo(navRef.current, { height: '7.5vh', opacity: 0, }, { height: '50px', duration: 0.625, ease: 'power2.inOut', opacity: 1, });
+        gsap.fromTo(aboutRef.current, { backgroundColor: '#28262C' }, { duration: 1.25, backgroundColor: '#F0F7F4' });
+      } else {
+        gsap.to(navRef.current, { opacity: 0, duration: 0.625, ease: 'power2.inOut' });
       }
     }, {
-      threshold: 0.25 // This means "when 10% of the target element is visible"
+      threshold: 0.9 // This means "when 10% of the target element is visible"
     });
 
     observer.observe(aboutRef.current);
@@ -26,8 +29,8 @@ export const About = () => {
   }, []);
 
   return (
-    <div ref={aboutRef} className="bg-mint-cream w-full h-screen flex flex-col items-center justify-center text-center">
-      <nav ref={navRef} className="bg-raisin-black h-12 w-full fixed top-0">
+    <div ref={aboutRef} className="bg-raisin-black w-full h-screen flex flex-col items-center justify-center text-center">
+      <nav ref={navRef} className="bg-raisin-black h-12 w-full fixed top-0 opacity-0">
         <ul className="flex gap-8 h-full justify-center items-center">
           <li>
             <a
