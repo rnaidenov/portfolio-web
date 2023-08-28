6a72f9604c4115efd037a5ff9cd807c45ccac213
Radoslav Naydenov <radoslav.naydenov@homagames.com>
2023-08-28 08:52:48 +0200

    Update mobile view | requires refactoring

--- src/app/components/info/info.tsx
+++ src/app/components/info/info.tsx
@@ -35,13 +35,13 @@ export const Info: React.FC<InfoProps> = ({ isNavHovered }) => {
   }, [isNavHovered, shouldShow])
 
   return (
-    <div className={`hidden md:flex justify-between items-end absolute ${shouldShow ? 'visible' : 'invisible'} uppercase w-full bottom-0 left-0 pb-4 px-8`}>
+    <div className={`h-1/2 overflow-hidden md:h-unset flex justify-between items-end absolute ${shouldShow ? 'visible' : 'invisible'} uppercase w-full bottom-0 left-0 pb-4 px-8`}>
       <div className="flex gap-8">
         <p className={`flex gap-2 ${isNavHovered ? 'text-raisin-black' : 'text-mint-cream'}`}>Work:</p>
-        <ul className={`flex items-center gap-2 ${isNavHovered ? 'text-outer-space' : 'text-flamy-orange'}`}>
+        <ul className={`relative flex flex-col items-start md:flex-row md:items-center gap-2 ${isNavHovered ? 'text-outer-space' : 'text-flamy-orange'}`}>
           <motion.ul
             id='work'
-            className={`${styles.mask} absolute flex items-center gap-2 ${isNavHovered ? 'text-outer-space' : 'text-flamy-orange'}`}
+            className={`flex-col items-start md:flex-row md:items-center ${styles.mask} absolute flex items-center gap-2 ${isNavHovered ? 'text-outer-space' : 'text-flamy-orange'}`}
             ref={maskRef}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
@@ -53,11 +53,11 @@ export const Info: React.FC<InfoProps> = ({ isNavHovered }) => {
             <li>
               <a href="https://fest-with-me.deno.dev/" target="_blank">FestWithMe</a>
             </li>
-            <span> | </span>
+            <span className="hidden md:inline-block"> | </span>
             <li>
               <a href="https://mintvalentine.xyz/" target="_blank">mintvalentine.xyz</a>
             </li>
-            <span> | </span>
+            <span className="hidden md:inline-block"> | </span>
             <li>
               <a href="https://kremenarov.com/" target="_blank">kremenarov.com</a>
             </li>
@@ -66,11 +66,11 @@ export const Info: React.FC<InfoProps> = ({ isNavHovered }) => {
           <li>
             FestWithMe
           </li>
-          <span> | </span>
+          <span className="hidden md:inline-block"> | </span>
           <li>
             mintvalentine.xyz
           </li>
-          <span> | </span>
+          <span className="hidden md:inline-block"> | </span>
           <li>
             kremenarov.com
           </li>
@@ -81,14 +81,14 @@ export const Info: React.FC<InfoProps> = ({ isNavHovered }) => {
         <p className='-rotate-90 absolute top-0 bottom-0 left-0'>
           Contacts:
         </p>
-        <div className={`absolute bottom-full right-0 flex flex-col items-end gap-4 pb-4 ${isNavHovered ? 'text-outer-space' : 'text-flamy-orange'}`}>
+        <div className={`absolute bottom-full right-0 flex flex-col items-end gap-4 pb-4`}>
           <a
             id='phone'
             href="tel:+359876703632"
             target="_blank"
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-            className={`transition transition-all transition-500 ${hovered == 'phone' ? 'text-outer-space scale-95' : 'text-flamy-orange scale-100'}`}
+            className={`transition transition-all transition-500 ${(hovered == 'phone' || isNavHovered) ? 'text-outer-space scale-95' : 'text-flamy-orange scale-100'}`}
           >
             <svg fill="currentColor" width="40px" height="40px" viewBox="0 0 56 56" xmlns="http://www.w3.org/2000/svg">
               <path d="M 27.9999 51.9062 C 41.0546 51.9062 51.9063 41.0547 51.9063 28.0000 C 51.9063 14.9219 41.0312 4.0938 27.9765 4.0938 C 14.8983 4.0938 4.0937 14.9219 4.0937 28.0000 C 4.0937 41.0547 14.9218 51.9062 27.9999 51.9062 Z M 21.8827 33.8360 C 16.0702 28.0469 12.3671 20.6640 16.7499 16.2813 C 17.0077 16.0234 17.2890 15.7656 17.5468 15.5078 C 18.8827 14.2422 20.1718 14.3125 21.3202 15.9297 L 24.3671 20.2656 C 25.3983 21.7656 25.1405 22.6094 24.0390 23.7813 L 23.0780 24.8360 C 22.7265 25.1640 22.8671 25.6094 23.0312 25.8906 C 23.4765 26.7344 24.7421 28.2344 26.1014 29.5938 C 27.5077 31.0000 28.9374 32.1953 29.8280 32.6875 C 30.1562 32.875 30.6249 32.9219 30.9296 32.6406 L 31.9374 31.6797 C 33.0624 30.5781 33.9765 30.2969 35.4296 31.3281 C 37.4452 32.7578 38.6640 33.6016 39.8593 34.4219 C 41.3358 35.5000 41.6874 36.8360 40.1874 38.1953 C 39.9296 38.4531 39.6952 38.7344 39.4374 38.9922 C 35.0546 43.3516 27.6952 39.6484 21.8827 33.8360 Z" />
@@ -101,7 +101,7 @@ export const Info: React.FC<InfoProps> = ({ isNavHovered }) => {
             target="_blank"
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-            className={`transition transition-all transition-500 ${hovered == 'linkedin' ? 'text-outer-space scale-95' : 'text-flamy-orange scale-100'}`}
+            className={`transition transition-all transition-500 ${(hovered == 'linkedin' || isNavHovered) ? 'text-outer-space scale-95' : 'text-flamy-orange scale-100'}`}
           >
             <svg fill="currentColor" width="40px" height="40px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
               viewBox="-143 145 512 512">
